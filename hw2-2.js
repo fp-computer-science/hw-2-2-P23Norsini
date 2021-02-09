231b40d4a745d254876aed934c41b8a7ccd3d6e4
P23Norsini <77790897+P23Norsini@users.noreply.github.com>
2021-02-09 09:01:22 -0500

    Create hw2-2.js

---
+++ hw2-2.js
@@ -0,0 +1,28 @@
+// Author: Noah Orsini (AMDG) 2/5/2021
+
+var baseAttack =20;
+var baseDefense =20;
+
+var warriorAttack
+var warriorDefense
+
+warriorAttack = baseAttack - 2
+warriorDefense = baseDefense * 20
+
+console.log("The warriors's attack is: "+ warriorAttack +" and the warriorDefense is: "warriorDefense");
+
+var rangerAttack
+var rangerDefense
+
+rangerAttack = baseAttack * 3;
+rangerDefense = baseDefense - 1;
+
+console.log("The warriors's attack is: "+ rangerAttack +" and the warriorDefense is: "rangerDefense");
+
+var mageAttack
+var mageDefense
+
+mageAttack = baseAttack ÷ 3;
+mageDefense = baseDefense ÷ 3;
+
+console.log("The warriors's attack is: "+ mageAttack +" and the warriorDefense is: "mageDefense");
